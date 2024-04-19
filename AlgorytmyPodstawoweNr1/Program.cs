a383d24e4b0c80dc9a6f9e1bbcfdaaa62319e951
benosbenos <152185798+benosbenos@users.noreply.github.com>
2024-04-19 11:34:46 +0200

    Add files via upload

---
+++ AlgorytmyPodstawoweNr1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static Sort sort = new Sort();
+    public static void Main()
+    {
+        List<int> tablica = new List<int>();
+
+        for (int i = 0; i < 11; i++ )  {
+            Console.WriteLine("podaj liczbe do tablicy która potem będzie uporządkowana");
+            tablica.Add(int.Parse(Console.ReadLine()));
+        
+        }
+        sort.sort(tablica);
+        Console.WriteLine("posortowana tablica");
+        sort.wypiszTablice(tablica);
+    }
+
+}
---
+++ AlgorytmyPodstawoweNr1/Sort.cs
@@ -0,0 +1,58 @@
+﻿
+public class Sort
+{
+
+    /**************************************
+     * nazwa funkcji:        <sort>*
+     * parametry wejściowe: <nazwa parametru> -<co przechowuje>*
+     * wartość zwracana:     <co zwraca funkcja–opis>*
+     * autor:                <Mikołaj Wieczorek 3D>* 
+     **************************************/
+    public void sort(List<int> tablica)
+    {
+        int n = tablica.Count;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (tablica[j] < tablica[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = tablica[min_idx];
+            tablica[min_idx] = tablica[i];
+            tablica[i] = temp;
+        }
+    }
+
+    // Prints the array
+
+    /**************************************
+     * nazwa funkcji:        <wypiszTablice>*
+     * parametry wejściowe: <nazwa parametru> -<co przechowuje>*
+     * wartość zwracana:     <co zwraca funkcja–opis>*
+     * autor:                <Mikołaj Wieczorek 3D>* 
+     **************************************/
+    public void wypiszTablice(List<int> tablica)
+    {
+        int n = tablica.Count;
+        for (int i = 0; i < n; ++i)
+            Console.Write(tablica[i] + " ");
+        Console.WriteLine();
+    }
+
+    // Driver code 
+
+    /**************************************
+     * nazwa funkcji:        <Main>*
+     * parametry wejściowe: <nazwa parametru> -<co przechowuje>*
+     * wartość zwracana:     <co zwraca funkcja–opis>*
+     * autor:                <Mikołaj Wieczorek 3D>* 
+     **************************************/
+
+}
+
